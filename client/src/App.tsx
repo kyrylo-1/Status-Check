96ab42505f627dddd77bbfc42079e0f558c53964
Kyrylo Avramenko <wkyrylo@gmail.com>
2020-09-21 18:58:06 -0700

    Setup tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 import { StatusCreator } from './components/StatusCreator';
 
 export const App = () => {
   return (
-    <div className="App">
+    <div>
       <StatusCreator />
     </div>
   );
--- client/src/components/StatusCreator.tsx
+++ client/src/components/StatusCreator.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
-let a = 3;
 export const StatusCreator = () => {
-  return <div></div>;
+  return (
+    <div className="container mx-auto">
+      <span>What did you do yesterday?</span>
+      <input
+        className="bg-white focus:outline-none focus:shadow-outline
+          border border-gray-300 rounded-lg py-1 px-2 h-16 block w-50 appearance-none leading-normal"
+        type="text"
+      ></input>
+      <button className="btn btn-blue mt-3">Button</button>
+    </div>
+  );
 };
